26e68ab1e5aff4b790c2804af06908919e71fcf3
Matthew L Daniel <mdaniel@atomwise.com>
2019-04-10 11:05:41 -0700

    Move Healthz out of Accesses

--- main.go
+++ main.go
@@ -91,7 +91,7 @@ func (a *Accesses) CostDataModelRange(w http.ResponseWriter, r *http.Request, ps
 	w.Write(wrapData(data, err))
 }
 
-func (a *Accesses) Healthz(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+func Healthz(w http.ResponseWriter, _ *http.Request, _ httprouter.Params) {
 	w.WriteHeader(200)
 	w.Header().Set("Content-Length", "0")
 	w.Header().Set("Content-Type", "text/plain")
@@ -197,7 +197,7 @@ func main() {
 	router := httprouter.New()
 	router.GET("/costDataModel", a.CostDataModel)
 	router.GET("/costDataModelRange", a.CostDataModelRange)
-	router.GET("/healthz", a.Healthz)
+	router.GET("/healthz", Healthz)
 	router.POST("/refreshPricing", a.RefreshPricingData)
 
 	rootMux := http.NewServeMux()
