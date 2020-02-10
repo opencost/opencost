51d1ec80de91a29c2e14988318f89ef3523c4f9d
AjayTripathy <4tripathy@gmail.com>
2020-02-10 14:13:05 -0800

    clean up logs, profiler

--- costmodel/costmodel.go
+++ costmodel/costmodel.go
@@ -2411,15 +2411,15 @@ func QueryRange(cli prometheusClient.Client, query string, start, end time.Time,
 
 	resp, body, warnings, err := cli.Do(context.Background(), req)
 	for _, w := range warnings {
-		klog.V(3).Infof("Warning '%s' fetching query '%s'", w, query)
+		klog.V(3).Infof("[Warning] '%s' fetching query '%s'", w, query)
 	}
 	if err != nil {
-		return nil, fmt.Errorf("Error %s fetching query %s", err.Error(), query)
+		return nil, fmt.Errorf("[Error] %s fetching query %s", err.Error(), query)
 	}
 	var toReturn interface{}
 	err = json.Unmarshal(body, &toReturn)
 	if err != nil {
-		return nil, fmt.Errorf("%d Error %s fetching query %s", resp.StatusCode, err.Error(), query)
+		return nil, fmt.Errorf("[Error] %d %s fetching query %s", resp.StatusCode, err.Error(), query)
 	}
 	return toReturn, err
 }
