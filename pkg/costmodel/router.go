ce530a7b9dd1f487a0f456e5caca7f8835d90611
Matt Bolt <mbolt35@gmail.com>
2020-09-24 16:09:38 -0400

    Add environment variable for logging queries to a configurable file.

--- pkg/costmodel/router.go
+++ pkg/costmodel/router.go
@@ -743,7 +743,7 @@ func Initialize(additionalConfigWatchers ...ConfigWatchers) {
 		Address:      address,
 		RoundTripper: LongTimeoutRoundTripper,
 	}
-	promCli, _ := prom.NewRateLimitedClient(pc, queryConcurrency, dbBasicAuthUsername, dbBasicAuthPW, dbBearerToken)
+	promCli, _ := prom.NewRateLimitedClient(pc, queryConcurrency, dbBasicAuthUsername, dbBasicAuthPW, dbBearerToken, env.GetQueryLoggingFile())
 
 	m, err := ValidatePrometheus(promCli, false)
 	if err != nil || m.Running == false {
@@ -977,7 +977,7 @@ func Initialize(additionalConfigWatchers ...ConfigWatchers) {
 				RoundTripper: thanosRT,
 			}
 
-			thanosCli, _ := prom.NewRateLimitedClient(thanosConfig, queryConcurrency, multiclusterDBBasicAuthUsername, multiclusterDBBasicAuthPW, multiClusterBearerToken)
+			thanosCli, _ := prom.NewRateLimitedClient(thanosConfig, queryConcurrency, multiclusterDBBasicAuthUsername, multiclusterDBBasicAuthPW, multiClusterBearerToken, env.GetQueryLoggingFile())
 
 			_, err = ValidatePrometheus(thanosCli, true)
 			if err != nil {
--- pkg/prom/prom.go
+++ pkg/prom/prom.go
@@ -4,6 +4,10 @@ import (
 	"context"
 	"net/http"
 	"net/url"
+	"os"
+	"time"
+
+	golog "log"
 
 	"github.com/kubecost/cost-model/pkg/log"
 	"github.com/kubecost/cost-model/pkg/util"
@@ -20,6 +24,7 @@ type RateLimitedPrometheusClient struct {
 	username    string
 	password    string
 	bearerToken string
+	fileLogger  *golog.Logger
 }
 
 // requestCounter is used to determine if the prometheus client keeps track of
@@ -31,7 +36,7 @@ type requestCounter interface {
 
 // NewRateLimitedClient creates a prometheus client which limits the number of concurrent outbound
 // prometheus requests.
-func NewRateLimitedClient(config prometheus.Config, maxConcurrency int, username, password, bearerToken string) (prometheus.Client, error) {
+func NewRateLimitedClient(config prometheus.Config, maxConcurrency int, username, password, bearerToken string, queryLogFile string) (prometheus.Client, error) {
 	c, err := prometheus.NewClient(config)
 	if err != nil {
 		return nil, err
@@ -41,6 +46,21 @@ func NewRateLimitedClient(config prometheus.Config, maxConcurrency int, username
 	requests := util.NewAtomicInt32(0)
 	outbound := util.NewAtomicInt32(0)
 
+	var logger *golog.Logger
+	if queryLogFile != "" {
+		exists, err := util.FileExists(queryLogFile)
+		if exists {
+			os.Remove(queryLogFile)
+		}
+
+		f, err := os.OpenFile(queryLogFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			log.Infof("Failed to open queryLogFile: %s for query logging: %s", queryLogFile, err)
+		} else {
+			logger = golog.New(f, "query-log", golog.LstdFlags)
+		}
+	}
+
 	return &RateLimitedPrometheusClient{
 		client:      c,
 		limiter:     limiter,
@@ -49,6 +69,7 @@ func NewRateLimitedClient(config prometheus.Config, maxConcurrency int, username
 		username:    username,
 		password:    password,
 		bearerToken: bearerToken,
+		fileLogger:  logger,
 	}, nil
 }
 
@@ -62,6 +83,18 @@ func LogPrometheusClientState(client prometheus.Client) {
 
 		log.Infof("Outbound Requests: %d, Queued Requests: %d, Total Requests: %d", outbound, queued, total)
 	}
+}
+
+// LogQueryRequest logs the query that was send to prom/thanos with the time in queue and total time after being sent
+func LogQueryRequest(l *golog.Logger, req *http.Request, queueTime time.Duration, sendTime time.Time) {
+	if l == nil {
+		return
+	}
+	qp := util.NewQueryParams(req.URL.Query())
+	query := qp.Get("query", "<Unknown>")
+
+	totalSendDuration := time.Since(sendTime)
+	l.Printf("[Queue: %fs, Outbound: %fs][Query: %s]\n", queueTime.Seconds(), totalSendDuration.Seconds(), query)
 }
 
 // TotalRequests returns the total number of requests that are either waiting to be sent and/or
@@ -90,6 +123,9 @@ func (rlpc *RateLimitedPrometheusClient) Do(ctx context.Context, req *http.Reque
 		token := "Bearer " + rlpc.bearerToken
 		req.Header.Add("Authorization", token)
 	}
+
+	start := time.Now()
+
 	// Increment the total request counter first
 	rlpc.requests.Increment()
 	defer rlpc.requests.Decrement()
@@ -97,6 +133,10 @@ func (rlpc *RateLimitedPrometheusClient) Do(ctx context.Context, req *http.Reque
 	// Acquire mutex based on concurrency limiter
 	rlpc.limiter.Acquire()
 	defer rlpc.limiter.Return()
+
+	timeInQueue := time.Since(start)
+	roundTripStart := time.Now()
+	defer LogQueryRequest(rlpc.fileLogger, req, timeInQueue, roundTripStart)
 
 	// Increment outbound once mutex acquired
 	rlpc.outbound.Increment()
