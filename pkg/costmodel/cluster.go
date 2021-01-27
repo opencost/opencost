5c4de223ccaf4ecf83972ede598ae1729a8e2d46
Michael Dresser <michael@kubecost.com>
2021-01-27 09:06:34 -0500

    Added tests for CPU cost adjustment for e2

--- pkg/costmodel/cluster.go
+++ pkg/costmodel/cluster.go
@@ -394,6 +394,9 @@ type Node struct {
 	Labels       map[string]string
 }
 
+// GKE lies about the number of cores e2 nodes have. This table
+// contains a mapping from node type -> actual CPU cores
+// for those cases.
 var partialCPUMap = map[string]float64{
 	"e2-micro":  0.25,
 	"e2-small":  0.5,
