e5de0112b2cd7f3be9e67de0c3017f421094860d
Neal Ormsbee <neal.ormsbee@gmail.com>
2021-03-19 16:53:49 -0600

    Moved spa_index.jsx to Reports.js

--- ui/src/app.js
+++ ui/src/app.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-import Reports from './spa_index.jsx';
+import Reports from './Reports.js';
 
 function ReportsPage() {
   return (
