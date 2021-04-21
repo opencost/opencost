d0be0a4f523b2833ef6f5653a3c4fa2a51899b43
Neal Ormsbee <neal.ormsbee@gmail.com>
2021-04-21 10:53:32 -0600

    Fixed a bad URL in the allocations UI

--- ui/src/services/allocation.js
+++ ui/src/services/allocation.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 class AllocationService {
-  BASE_URL = 'http://localhost:9090/model/allocation';
+  BASE_URL = 'http://localhost:9090/allocation';
 
   async fetchAllocation(win, aggregate, options) {
     const { accumulate, filters, } = options;
