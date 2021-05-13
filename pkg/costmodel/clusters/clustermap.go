bf66fe79da42b27bf354d2d0aa846af40595ac3b
Ajay Tripathy <4tripathy@gmail.com>
2021-05-12 21:59:52 -0700

    revert clustermap

--- pkg/costmodel/clusters/clustermap.go
+++ pkg/costmodel/clusters/clustermap.go
@@ -183,7 +183,7 @@ func (pcm *PrometheusClusterMap) refreshClusters() {
 	updated, err := pcm.loadClusters()
 	if err != nil {
 		log.Errorf("Failed to load cluster info via query after %d retries", LoadRetries)
-		updated = make(map[string]*ClusterInfo) // If we fail to query prometheus, register no clusters, as we use this to gauge availability of clusters.
+		return
 	}
 
 	pcm.lock.Lock()
