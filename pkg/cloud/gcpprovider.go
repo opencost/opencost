0195934426ae8ad1185b0778712cdd2c3af04a66
Michael Dresser <michaelmdresser@gmail.com>
2021-10-01 15:36:31 -0400

    Refactor and add tests

--- pkg/cloud/gcpprovider.go
+++ pkg/cloud/gcpprovider.go
@@ -1395,6 +1395,33 @@ func (gcp *gcpKey) GPUType() string {
 	return ""
 }
 
+func parseGCPInstanceTypeLabel(it string) string {
+	var instanceType string
+
+	splitByDash := strings.Split(it, "-")
+
+	// GKE nodes are labeled with the GCP instance type, but users can deploy on GCP
+	// with tools like K3s, whose instance type labels will be "k3s". This logic
+	// avoids a panic in the slice operation then there are no dashes (-) in the
+	// instance type label value.
+	if len(splitByDash) < 2 {
+		instanceType = "unknown"
+	} else {
+		instanceType = strings.ToLower(strings.Join(splitByDash[:2], ""))
+		if instanceType == "n1highmem" || instanceType == "n1highcpu" {
+			instanceType = "n1standard" // These are priced the same. TODO: support n1ultrahighmem
+		} else if instanceType == "n2highmem" || instanceType == "n2highcpu" {
+			instanceType = "n2standard"
+		} else if instanceType == "e2highmem" || instanceType == "e2highcpu" {
+			instanceType = "e2standard"
+		} else if strings.HasPrefix(instanceType, "custom") {
+			instanceType = "custom" // The suffix of custom does not matter
+		}
+	}
+
+	return instanceType
+}
+
 // GetKey maps node labels to information needed to retrieve pricing data
 func (gcp *gcpKey) Features() string {
 	var instanceType string
@@ -1403,26 +1430,7 @@ func (gcp *gcpKey) Features() string {
 		log.DedupedErrorf(1, "Missing or Unknown 'node.kubernetes.io/instance-type' node label")
 		instanceType = "unknown"
 	} else {
-		splitByDash := strings.Split(it, "-")
-
-		// GKE nodes are labeled with the GCP instance type, but users can deploy on GCP
-		// with tools like K3s, whose instance type labels will be "k3s". This logic
-		// avoids a panic in the slice operation then there are no dashes (-) in the
-		// instance type label value.
-		if len(splitByDash) < 3 {
-			instanceType = "unknown"
-		} else {
-			instanceType = strings.ToLower(strings.Join(splitByDash[:2], ""))
-			if instanceType == "n1highmem" || instanceType == "n1highcpu" {
-				instanceType = "n1standard" // These are priced the same. TODO: support n1ultrahighmem
-			} else if instanceType == "n2highmem" || instanceType == "n2highcpu" {
-				instanceType = "n2standard"
-			} else if instanceType == "e2highmem" || instanceType == "e2highcpu" {
-				instanceType = "e2standard"
-			} else if strings.HasPrefix(instanceType, "custom") {
-				instanceType = "custom" // The suffix of custom does not matter
-			}
-		}
+		instanceType = parseGCPInstanceTypeLabel(it)
 	}
 
 	r, _ := util.GetRegion(gcp.Labels)
