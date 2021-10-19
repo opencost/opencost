86094714835c4d2b30ea1d10fb523b74526276e4
Sean Holcomb <seanholcomb@gmail.com>
2021-10-19 13:16:12 -0700

    json marshaling update

--- pkg/kubecost/asset_unmarshal_test.go
+++ pkg/kubecost/asset_unmarshal_test.go
@@ -18,8 +18,15 @@ func TestAny_Unmarshal(t *testing.T) {
 		Cluster:    "cluster1",
 		ProviderID: "any1",
 	})
+	any1.SetAssetPricingModels(&AssetPricingModels{
+		Preemptible:      0.5,
+		ReservedInstance: 0.1,
+		SavingsPlan:      0.3,
+	})
 	any1.Cost = 9.0
 	any1.SetAdjustment(1.0)
+	any1.SetCredit(-1.0)
+	any1.SetDiscount(0.1)
 
 	bytes, _ := json.Marshal(any1)
 
@@ -40,6 +47,9 @@ func TestAny_Unmarshal(t *testing.T) {
 	if !any1.labels.Equal(any2.labels) {
 		t.Fatalf("Any Unmarshal: labels mutated in unmarshal")
 	}
+	if !any1.assetPricingModels.Equal(any2.assetPricingModels) {
+		t.Fatalf("Any Unmarshal: assetPricingModels mutated in unmarshal")
+	}
 	if !any1.window.Equal(any2.window) {
 		t.Fatalf("Any Unmarshal: window mutated in unmarshal")
 	}
@@ -51,6 +61,12 @@ func TestAny_Unmarshal(t *testing.T) {
 	}
 	if any1.adjustment != any2.adjustment {
 		t.Fatalf("Any Unmarshal: adjustment mutated in unmarshal")
+	}
+	if any1.credit != any2.credit {
+		t.Fatalf("Any Unmarshal: credit mutated in unmarshal")
+	}
+	if any1.discount != any2.discount {
+		t.Fatalf("Any Unmarshal: discount mutated in unmarshal")
 	}
 	if any1.Cost != any2.Cost {
 		t.Fatalf("Any Unmarshal: cost mutated in unmarshal")
@@ -71,8 +87,16 @@ func TestCloud_Unmarshal(t *testing.T) {
 		"env":       "env1",
 		"product":   "product1",
 	})
+	cloud1.SetAssetPricingModels(&AssetPricingModels{
+		Preemptible:      0.0,
+		ReservedInstance: 0.1,
+		SavingsPlan:      0.3,
+	})
 	cloud1.Cost = 10.00
-	cloud1.Credit = -1.0
+	cloud1.SetCredit(-1.0)
+	cloud1.SetDiscount(.25)
+	cloud1.UsageType = "usageType1"
+	cloud1.UsageDetail = "usageDetail1"
 
 	bytes, _ := json.Marshal(cloud1)
 
@@ -93,6 +117,9 @@ func TestCloud_Unmarshal(t *testing.T) {
 	if !cloud1.labels.Equal(cloud2.labels) {
 		t.Fatalf("Cloud Unmarshal: labels mutated in unmarshal")
 	}
+	if !cloud1.assetPricingModels.Equal(cloud2.assetPricingModels) {
+		t.Fatalf("Cloud Unmarshal: assetPricingModels mutated in unmarshal")
+	}
 	if !cloud1.window.Equal(cloud2.window) {
 		t.Fatalf("Cloud Unmarshal: window mutated in unmarshal")
 	}
@@ -105,12 +132,22 @@ func TestCloud_Unmarshal(t *testing.T) {
 	if cloud1.adjustment != cloud2.adjustment {
 		t.Fatalf("Cloud Unmarshal: adjustment mutated in unmarshal")
 	}
+	if cloud1.credit != cloud2.credit {
+		t.Fatalf("Cloud Unmarshal: credit mutated in unmarshal")
+	}
+	if cloud1.discount != cloud2.discount {
+		t.Fatalf("Cloud Unmarshal: discount mutated in unmarshal")
+	}
 	if cloud1.Cost != cloud2.Cost {
 		t.Fatalf("Cloud Unmarshal: cost mutated in unmarshal")
 	}
-	if cloud1.Credit != cloud2.Credit {
-		t.Fatalf("Cloud Unmarshal: credit mutated in unmarshal")
+	if cloud1.UsageType != cloud2.UsageType {
+		t.Fatalf("Cloud Unmarshal: UsageType mutated in unmarshal")
 	}
+	if cloud1.UsageDetail != cloud2.UsageDetail {
+		t.Fatalf("Cloud Unmarshal: UsageDetail mutated in unmarshal")
+	}
+
 
 	// As a final check, make sure the above checks out
 	if !cloud1.Equal(cloud2) {
@@ -122,6 +159,7 @@ func TestCloud_Unmarshal(t *testing.T) {
 func TestClusterManagement_Unmarshal(t *testing.T) {
 
 	cm1 := NewClusterManagement("gcp", "cluster1", unmarshalWindow)
+	cm1.SetAssetPricingModels(nil)
 	cm1.Cost = 9.0
 
 	bytes, _ := json.Marshal(cm1)
@@ -143,8 +181,20 @@ func TestClusterManagement_Unmarshal(t *testing.T) {
 	if !cm1.labels.Equal(cm2.labels) {
 		t.Fatalf("ClusterManagement Unmarshal: labels mutated in unmarshal")
 	}
+	if !cm1.assetPricingModels.Equal(cm2.assetPricingModels) {
+		t.Fatalf("ClusterManagement Unmarshal: assetPricingModels mutated in unmarshal")
+	}
 	if !cm1.window.Equal(cm2.window) {
 		t.Fatalf("ClusterManagement Unmarshal: window mutated in unmarshal")
+	}
+	if cm1.adjustment != cm2.adjustment {
+		t.Fatalf("ClusterManagement Unmarshal: adjustment mutated in unmarshal")
+	}
+	if cm1.credit != cm2.credit {
+		t.Fatalf("ClusterManagement Unmarshal: credit mutated in unmarshal")
+	}
+	if cm1.discount != cm2.discount {
+		t.Fatalf("ClusterManagement Unmarshal: discount mutated in unmarshal")
 	}
 	if cm1.Cost != cm2.Cost {
 		t.Fatalf("ClusterManagement Unmarshal: cost mutated in unmarshal")
@@ -162,16 +212,20 @@ func TestDisk_Unmarshal(t *testing.T) {
 	hours := unmarshalWindow.Duration().Hours()
 
 	disk1 := NewDisk("disk1", "cluster1", "disk1", *unmarshalWindow.start, *unmarshalWindow.end, unmarshalWindow)
+	disk1.SetAssetPricingModels(&AssetPricingModels{})
 	disk1.ByteHours = 60.0 * gb * hours
 	disk1.Cost = 4.0
 	disk1.Local = 1.0
 	disk1.SetAdjustment(1.0)
+	disk1.SetDiscount(0)
+	disk1.SetCredit(-5.4)
 	disk1.Breakdown = &Breakdown{
 		Idle:   0.1,
 		System: 0.2,
 		User:   0.3,
 		Other:  0.4,
 	}
+	disk1.StorageClass = "storageClass1"
 
 	bytes, _ := json.Marshal(disk1)
 
@@ -192,6 +246,9 @@ func TestDisk_Unmarshal(t *testing.T) {
 	if !disk1.labels.Equal(disk2.labels) {
 		t.Fatalf("Disk Unmarshal: labels mutated in unmarshal")
 	}
+	if !disk1.assetPricingModels.Equal(disk2.assetPricingModels) {
+		t.Fatalf("Disk Unmarshal: assetPricingModels mutated in unmarshal")
+	}
 	if !disk1.window.Equal(disk2.window) {
 		t.Fatalf("Disk Unmarshal: window mutated in unmarshal")
 	}
@@ -207,11 +264,20 @@ func TestDisk_Unmarshal(t *testing.T) {
 	if disk1.adjustment != disk2.adjustment {
 		t.Fatalf("Disk Unmarshal: adjustment mutated in unmarshal")
 	}
+	if disk1.credit != disk2.credit {
+		t.Fatalf("Disk Unmarshal: credit mutated in unmarshal")
+	}
+	if disk1.discount != disk2.discount {
+		t.Fatalf("Disk Unmarshal: discount mutated in unmarshal")
+	}
 	if disk1.ByteHours != disk2.ByteHours {
 		t.Fatalf("Disk Unmarshal: ByteHours mutated in unmarshal")
 	}
 	if disk1.Cost != disk2.Cost {
 		t.Fatalf("Disk Unmarshal: cost mutated in unmarshal")
+	}
+	if disk1.StorageClass != disk2.StorageClass {
+		t.Fatalf("Disk Unmarshal: StorageClass mutated in unmarshal")
 	}
 
 	// Local from Disk is not marhsaled, and cannot be calculated from marshaled values.
@@ -224,8 +290,15 @@ func TestDisk_Unmarshal(t *testing.T) {
 func TestNetwork_Unmarshal(t *testing.T) {
 
 	network1 := NewNetwork("network1", "cluster1", "provider1", *unmarshalWindow.start, *unmarshalWindow.end, unmarshalWindow)
+	network1.SetAssetPricingModels(&AssetPricingModels{
+		Preemptible:      0.0,
+		ReservedInstance: 0.0,
+		SavingsPlan:      0.3,
+	})
 	network1.Cost = 4.0
 	network1.SetAdjustment(1.0)
+	network1.SetCredit(-2.2)
+	network1.SetDiscount(1.1)
 
 	bytes, _ := json.Marshal(network1)
 
@@ -246,6 +319,9 @@ func TestNetwork_Unmarshal(t *testing.T) {
 	if !network1.labels.Equal(network2.labels) {
 		t.Fatalf("Network Unmarshal: labels mutated in unmarshal")
 	}
+	if !network1.assetPricingModels.Equal(network2.assetPricingModels) {
+		t.Fatalf("Network Unmarshal: assetPricingModels mutated in unmarshal")
+	}
 	if !network1.window.Equal(network2.window) {
 		t.Fatalf("Network Unmarshal: window mutated in unmarshal")
 	}
@@ -257,6 +333,12 @@ func TestNetwork_Unmarshal(t *testing.T) {
 	}
 	if network1.adjustment != network2.adjustment {
 		t.Fatalf("Network Unmarshal: adjustment mutated in unmarshal")
+	}
+	if network1.credit != network2.credit {
+		t.Fatalf("Network Unmarshal: credit mutated in unmarshal")
+	}
+	if network1.discount != network2.discount {
+		t.Fatalf("Network Unmarshal: discount mutated in unmarshal")
 	}
 	if network1.Cost != network2.Cost {
 		t.Fatalf("Network Unmarshal: cost mutated in unmarshal")
@@ -274,13 +356,17 @@ func TestNode_Unmarshal(t *testing.T) {
 	hours := unmarshalWindow.Duration().Hours()
 
 	node1 := NewNode("node1", "cluster1", "provider1", *unmarshalWindow.start, *unmarshalWindow.end, unmarshalWindow)
+	node1.SetAssetPricingModels(&AssetPricingModels{
+		Preemptible:      1.0,
+		ReservedInstance: 0.0,
+		SavingsPlan:      0.0,
+	})
 	node1.CPUCoreHours = 1.0 * hours
 	node1.RAMByteHours = 2.0 * gb * hours
 	node1.GPUHours = 0.0 * hours
 	node1.GPUCost = 0.0
 	node1.CPUCost = 8.0
 	node1.RAMCost = 4.0
-	node1.Discount = 0.3
 	node1.CPUBreakdown = &Breakdown{
 		Idle:   0.6,
 		System: 0.2,
@@ -294,6 +380,8 @@ func TestNode_Unmarshal(t *testing.T) {
 		Other:  0.0,
 	}
 	node1.SetAdjustment(1.6)
+	node1.SetDiscount(.8)
+	node1.SetCredit(-3.0)
 
 	bytes, _ := json.Marshal(node1)
 
@@ -314,6 +402,9 @@ func TestNode_Unmarshal(t *testing.T) {
 	if !node1.labels.Equal(node2.labels) {
 		t.Fatalf("Node Unmarshal: labels mutated in unmarshal")
 	}
+	if !node1.assetPricingModels.Equal(node2.assetPricingModels) {
+		t.Fatalf("Node Unmarshal: assetPricingModels mutated in unmarshal")
+	}
 	if !node1.window.Equal(node2.window) {
 		t.Fatalf("Node Unmarshal: window mutated in unmarshal")
 	}
@@ -331,6 +422,12 @@ func TestNode_Unmarshal(t *testing.T) {
 	}
 	if node1.adjustment != node2.adjustment {
 		t.Fatalf("Node Unmarshal: adjustment mutated in unmarshal")
+	}
+	if node1.credit != node2.credit {
+		t.Fatalf("Node Unmarshal: credit mutated in unmarshal")
+	}
+	if node1.discount != node2.discount {
+		t.Fatalf("Node Unmarshal: dicount mutated in unmarshal")
 	}
 	if node1.NodeType != node2.NodeType {
 		t.Fatalf("Node Unmarshal: NodeType mutated in unmarshal")
@@ -356,12 +453,6 @@ func TestNode_Unmarshal(t *testing.T) {
 	if node1.RAMCost != node2.RAMCost {
 		t.Fatalf("Node Unmarshal: RAMCost mutated in unmarshal")
 	}
-	if node1.Discount != node2.Discount {
-		t.Fatalf("Node Unmarshal: Discount mutated in unmarshal")
-	}
-	if node1.Preemptible != node2.Preemptible {
-		t.Fatalf("Node Unmarshal: Preemptible mutated in unmarshal")
-	}
 
 	// As a final check, make sure the above checks out
 	if !node1.Equal(node2) {
@@ -373,8 +464,15 @@ func TestNode_Unmarshal(t *testing.T) {
 func TestLoadBalancer_Unmarshal(t *testing.T) {
 
 	lb1 := NewLoadBalancer("loadbalancer1", "cluster1", "provider1", *unmarshalWindow.start, *unmarshalWindow.end, unmarshalWindow)
+	lb1.SetAssetPricingModels(&AssetPricingModels{
+		Preemptible:      0.0,
+		ReservedInstance: 0.0,
+		SavingsPlan:      1.0,
+	})
 	lb1.Cost = 12.0
 	lb1.SetAdjustment(4.0)
+	lb1.SetDiscount(0)
+	lb1.SetCredit(-0.001)
 
 	bytes, _ := json.Marshal(lb1)
 
@@ -395,6 +493,9 @@ func TestLoadBalancer_Unmarshal(t *testing.T) {
 	if !lb1.labels.Equal(lb2.labels) {
 		t.Fatalf("LoadBalancer Unmarshal: labels mutated in unmarshal")
 	}
+	if !lb1.assetPricingModels.Equal(lb2.assetPricingModels) {
+		t.Fatalf("LoadBalancer Unmarshal: assetPricingModels mutated in unmarshal")
+	}
 	if !lb1.window.Equal(lb2.window) {
 		t.Fatalf("LoadBalancer Unmarshal: window mutated in unmarshal")
 	}
@@ -406,6 +507,12 @@ func TestLoadBalancer_Unmarshal(t *testing.T) {
 	}
 	if lb1.adjustment != lb2.adjustment {
 		t.Fatalf("LoadBalancer Unmarshal: adjustment mutated in unmarshal")
+	}
+	if lb1.credit != lb2.credit {
+		t.Fatalf("LoadBalancer Unmarshal: credit mutated in unmarshal")
+	}
+	if lb1.discount != lb2.discount {
+		t.Fatalf("LoadBalancer Unmarshal: discount mutated in unmarshal")
 	}
 	if lb1.Cost != lb2.Cost {
 		t.Fatalf("LoadBalancer Unmarshal: cost mutated in unmarshal")
@@ -421,6 +528,14 @@ func TestLoadBalancer_Unmarshal(t *testing.T) {
 func TestSharedAsset_Unmarshal(t *testing.T) {
 
 	sa1 := NewSharedAsset("sharedasset1", unmarshalWindow)
+	sa1.SetAssetPricingModels(&AssetPricingModels{
+		Preemptible:      0.0,
+		ReservedInstance: 0.0,
+		SavingsPlan:      1.0,
+	})
+	sa1.SetAdjustment(0.1)
+	sa1.SetCredit(1.0)
+	sa1.SetDiscount(.001)
 	sa1.Cost = 7.0
 
 	bytes, _ := json.Marshal(sa1)
@@ -442,8 +557,20 @@ func TestSharedAsset_Unmarshal(t *testing.T) {
 	if !sa1.labels.Equal(sa2.labels) {
 		t.Fatalf("SharedAsset Unmarshal: labels mutated in unmarshal")
 	}
+	if !sa1.assetPricingModels.Equal(sa2.assetPricingModels) {
+		t.Fatalf("SharedAsset Unmarshal: assetPricingModels mutated in unmarshal")
+	}
 	if !sa1.window.Equal(sa2.window) {
 		t.Fatalf("SharedAsset Unmarshal: window mutated in unmarshal")
+	}
+	if sa1.adjustment != sa2.adjustment {
+		t.Fatalf("SharedAsset Unmarshal: adjustment mutated in unmarshal")
+	}
+	if sa1.credit != sa2.credit {
+		t.Fatalf("SharedAsset Unmarshal: credit mutated in unmarshal")
+	}
+	if sa1.discount != sa2.discount {
+		t.Fatalf("SharedAsset Unmarshal: discount mutated in unmarshal")
 	}
 	if sa1.Cost != sa2.Cost {
 		t.Fatalf("SharedAsset Unmarshal: cost mutated in unmarshal")
