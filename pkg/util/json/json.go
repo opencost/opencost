3a49162311c80aeeef03c523326a06291a8549bb
Matt Bolt <mbolt35@gmail.com>
2022-03-14 15:45:09 -0400

    Add json-iterator Unmarshal() back to our pipeline to avoid the memory duplication that occurs in go-json.
    (cherry picked from commit 64fb39b4d00d4790ab8e6e0451177de3e24f5aa7)

--- pkg/util/json/json.go
+++ pkg/util/json/json.go
@@ -4,10 +4,15 @@ import (
 	"encoding/json"
 
 	gojson "github.com/goccy/go-json"
+	jsoniter "github.com/json-iterator/go"
 )
 
+// Marshal leverages the go-json library for Marshalling JSON. This avoids a recursive
+// loop that occurs on marshal errors in json-iterator.
 var Marshal = gojson.Marshal
-var Unmarshal = gojson.Unmarshal
+
+// Unmarshal leverages the json-iterator library for Unmarshalling JSON.
+var Unmarshal = jsoniter.Unmarshal
 
 var NewEncoder = json.NewEncoder
 var NewDecoder = json.NewDecoder
