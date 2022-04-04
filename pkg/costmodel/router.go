2e540e461a21b0b2c7b4a0c92434ef2734fd1faf
Ryan Umstead <ryan.umstead@blackrock.com>
2022-04-04 08:57:44 -0400

    return quicker if not running in k8s

--- pkg/costmodel/router.go
+++ pkg/costmodel/router.go
@@ -1433,7 +1433,7 @@ func Initialize(additionalConfigWatchers ...*watcher.ConfigMapWatcher) *Accesses
 
 	// Kubernetes API setup
 	if err = env.AddK8sApiNoProxy(); err != nil {
-		klog.Errorf("unable to set k8s api server in no proxy: %w\n", err)
+		klog.Errorf("unable to set k8s api server in no proxy: %v\n", err)
 	}
 	var kc *rest.Config
 	if kubeconfig := env.GetKubeConfigPath(); kubeconfig != "" {
--- pkg/env/k8senv.go
+++ pkg/env/k8senv.go
@@ -6,6 +6,10 @@ func AddK8sApiNoProxy() error {
 	// get the k8s api server host
 	// https://github.com/kubernetes/kubernetes/blob/691d4c3989f18e0be22c4499d22eff95d516d32b/pkg/kubelet/kubelet_pods.go#L586
 	k8sIP := Get("KUBERNETES_SERVICE_HOST", "")
+	// if we aren't running in k8s, do nothing
+	if k8sIP == "" {
+		return nil
+	}
 	// get any existing no proxy and append to it
 	noProxy := Get("no_proxy", "")
 	if noProxy != "" {
