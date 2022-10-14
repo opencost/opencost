308e28b5fa3f7c336ccdc775981a5afc6900453a
Niko Kovacevic <nikovacevic@gmail.com>
2022-10-14 10:55:10 -0600

    WIP: CloudCost types with bingen codec update

--- pkg/kubecost/cloudcostaggregate.go
+++ pkg/kubecost/cloudcostaggregate.go
@@ -1,14 +1,16 @@
 package kubecost
 
-import "time"
+import (
+	"time"
+)
 
 // CloudCostAggregateProperties unique property set for CloudCostAggregate within a window
 type CloudCostAggregateProperties struct {
-	Provider string
-	Account  string
-	Project  string
-	Service  string
-	Label    string
+	Provider   string
+	Account    string
+	Project    string
+	Service    string
+	LabelValue string
 }
 
 // CloudCostAggregate represents an aggregation of Billing Integration data on the properties listed
@@ -23,12 +25,31 @@ type CloudCostAggregate struct {
 	Credit            float64
 }
 
+// TODO Integration here or on Properties?
+// TODO LabelName here or on Properties?
 type CloudCostAggregateSet struct {
-	CloudCosts map[string]*CloudCostAggregate
-	Window     Window
-	TotalCost  float64
+	CloudCostAggregates map[string]*CloudCostAggregate
+	Integration         string
+	LabelName           string
+	Window              Window
+}
+
+func (ccas *CloudCostAggregateSet) Clone() *CloudCostAggregateSet {
+	// TODO
+	return nil
+}
+
+func (ccas *CloudCostAggregateSet) IsEmpty() bool {
+	// TODO
+	return true
 }
 
 func (ccas *CloudCostAggregateSet) GetWindow() Window {
 	return ccas.Window
+}
+
+type CloudCostAggregateSetRange struct {
+	CloudCostAggregateSets []*CloudCostAggregateSet
+	Step                   time.Duration
+	Window                 Window
 }
