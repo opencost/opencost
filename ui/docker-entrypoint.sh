6d97b8185a6384f374f4a352f05581b7d87fbdb7
Logan Ballard <loganballard@gmail.com>
2023-02-19 10:13:02 -0500

    Adding parcel-cache to gitignore Adding Docker-entrypoint script to UI docker container to allow for building the UI container with the ability to override the BASE_URL Modifying the dockerfile for the UI to containe the entrypoint and to no longer build the container with a BASE_URL baked in Adding a Makefile to the UI project to ease development Removing the default from the AllocationService within the UI to be a placeholder Dependencies updated themselves in package-lock
    Signed-off-by: Logan Ballard <loganballard@gmail.com>
    Signed-off-by: thomasvn <thomasnguyen96@gmail.com>

---
+++ ui/docker-entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e
+
+if [[ ! -z "$BASE_URL_OVERRIDE" ]]; then
+    echo "running with BASE_URL=${BASE_URL_OVERRIDE}"
+    sed -i "s^{PLACEHOLDER_BASE_URL}^$BASE_URL_OVERRIDE^g" /var/www/*.js
+else 
+    echo "running with BASE_URL=${BASE_URL}"
+    sed -i "s^{PLACEHOLDER_BASE_URL}^$BASE_URL^g" /var/www/*.js
+fi
+
+exec "$@"
