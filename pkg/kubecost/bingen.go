b5fcb1adc79d32105337be17af2d5faa48b67835
Alex Meijer <ameijer@users.noreply.github.com>
2023-06-08 14:35:16 -0400

    Imp ns labels annots (#1896) (#1979)
    * [CORE-140] Implementation of nsLabel/nsAnnotation
    
    Signed-off-by: Alex Meijer <ameijer@kubecost.com>
    
    * remove commented code
    
    Signed-off-by: Alex Meijer <ameijer@kubecost.com>
    
    * updates to have namespace labels appear when aggregated
    
    Signed-off-by: Alex Meijer <ameijer@kubecost.com>
    
    * Add Intersection() tests
    
    Signed-off-by: Alex Meijer <ameijer@kubecost.com>
    
    * run bingen to set minimum version for these labels
    
    Signed-off-by: Alex Meijer <ameijer@kubecost.com>
    
    * rm agg meta
    
    Signed-off-by: Alex Meijer <ameijer@kubecost.com>
    
    * fix tests
    
    Signed-off-by: Alex Meijer <ameijer@kubecost.com>
    
    * add back in agg meta capability
    
    Signed-off-by: Alex Meijer <ameijer@kubecost.com>
    
    * add missing lines, always default to false
    
    Signed-off-by: Alex Meijer <ameijer@kubecost.com>
    
    ---------
    
    Signed-off-by: Alex Meijer <ameijer@kubecost.com>
    Signed-off-by: Alex Meijer <ameijer@users.noreply.github.com>
    (cherry picked from commit 1abc346db0b25a7a16ae292fa2cdd3f131793c8f)

--- pkg/kubecost/bingen.go
+++ pkg/kubecost/bingen.go
@@ -46,7 +46,7 @@ package kubecost
 // @bingen:end
 
 // Allocation Version Set: Includes Allocation pipeline specific resources
-// @bingen:set[name=Allocation,version=16]
+// @bingen:set[name=Allocation,version=17]
 // @bingen:generate:Allocation
 // @bingen:generate[stringtable]:AllocationSet
 // @bingen:generate:AllocationSetRange
