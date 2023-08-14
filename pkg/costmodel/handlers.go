5afe11575fa08a2281591fad1c098e8e41584695
Alex Meijer <ameijer@kubecost.com>
2023-08-14 10:35:47 -0400

    add filtering
    Signed-off-by: Alex Meijer <ameijer@kubecost.com>
    (cherry picked from commit 9c47e80987b3b2b59e1efa2a23ba8586d3b2e95d)

--- pkg/costmodel/handlers.go
+++ pkg/costmodel/handlers.go
@@ -6,6 +6,9 @@ import (
 
 	"github.com/julienschmidt/httprouter"
 	"github.com/opencost/opencost/pkg/env"
+	assetfilter "github.com/opencost/opencost/pkg/filter21/asset"
+	"github.com/opencost/opencost/pkg/filter21/ast"
+	"github.com/opencost/opencost/pkg/filter21/matcher"
 	"github.com/opencost/opencost/pkg/kubecost"
 	"github.com/opencost/opencost/pkg/util/httputil"
 )
@@ -28,6 +31,33 @@ func (a *Accesses) ComputeAssetsHandler(w http.ResponseWriter, r *http.Request, 
 	if err != nil {
 		http.Error(w, fmt.Sprintf("Error computing asset set: %s", err), http.StatusInternalServerError)
 		return
+	}
+	filterString := qp.Get("filter", "")
+
+	var filter kubecost.AssetMatcher
+	if filterString == "" {
+		filter = &matcher.AllPass[kubecost.Asset]{}
+	} else {
+		parser := assetfilter.NewAssetFilterParser()
+		tree, errParse := parser.Parse(filterString)
+		if errParse != nil {
+			http.Error(w, fmt.Sprintf("err parsing filter '%s': %v", ast.ToPreOrderShortString(tree), errParse), http.StatusBadRequest)
+		}
+		compiler := kubecost.NewAssetMatchCompiler()
+		var err error
+		filter, err = compiler.Compile(tree)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("err compiling filter '%s': %v", ast.ToPreOrderShortString(tree), err), http.StatusBadRequest)
+		}
+	}
+	if filter == nil {
+		http.Error(w, fmt.Sprintf("unexpected nil filter"), http.StatusInternalServerError)
+	}
+
+	for key, asset := range assetSet.Assets {
+		if !filter.Matches(asset) {
+			delete(assetSet.Assets, key)
+		}
 	}
 
 	w.Write(WrapData(assetSet, nil))
