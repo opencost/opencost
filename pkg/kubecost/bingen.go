a1440622c6fb4462c11369166ab21be0d24f6aad
Alex Meijer <ameijer@kubecost.com>
2023-08-28 08:58:21 -0400

    re-write to pass through allocations, not assets
    Signed-off-by: Alex Meijer <ameijer@kubecost.com>

--- pkg/kubecost/bingen.go
+++ pkg/kubecost/bingen.go
@@ -46,7 +46,7 @@ package kubecost
 // @bingen:end
 
 // Allocation Version Set: Includes Allocation pipeline specific resources
-// @bingen:set[name=Allocation,version=19]
+// @bingen:set[name=Allocation,version=20]
 // @bingen:generate:Allocation
 // @bingen:generate[stringtable]:AllocationSet
 // @bingen:generate:AllocationSetRange
