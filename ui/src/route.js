9c80b83dd4c3cdb8ce1aba9ed49574c289a65d3f
jjarrett21 <jjarrett21@gmail.com>
2023-10-26 10:08:00 -0500

    Sidebar nav and header changes

--- ui/src/route.js
+++ ui/src/route.js
@@ -3,13 +3,15 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Reports from "./Reports.js";
 import CloudCostReports from "./CloudCostReports.js";
-import Sidebar from "./components/Sidebar.js";
 
 const Routes = () => {
   return (
     <Router>
       <Switch>
         <Route exact path="/">
+          <Reports />
+        </Route>
+        <Route exact path="/allocation">
           <Reports />
         </Route>
         <Route exact path="/cloud">
