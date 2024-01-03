adcbb626419e672b453e22ba489554123aee6296
Michael Dresser <michaelmdresser@gmail.com>
2024-01-03 12:01:22 -0800

    Add further unit test
    Signed-off-by: Michael Dresser <michaelmdresser@gmail.com>

--- pkg/util/fileutil/locks_test.go
+++ pkg/util/fileutil/locks_test.go
@@ -1,9 +1,30 @@
 package fileutil
 
 import (
+	"os"
 	"path/filepath"
 	"testing"
 )
+
+// Make sure read works on file created without locking logic
+func TestReadLocked(t *testing.T) {
+	toWrite := "hello world"
+	dir := t.TempDir()
+	filename := filepath.Join(dir, "test.txt")
+	if err := os.WriteFile(filename, []byte(toWrite), 0600); err != nil {
+		t.Fatalf("failed to write test data: %s", err)
+	}
+
+	read, err := ReadLocked(filename)
+	if err != nil {
+		t.Fatalf("Failed to read: %s", err)
+	}
+	sread := string(read)
+
+	if toWrite != sread {
+		t.Errorf("Expected read data to be '%s' but was '%s'", toWrite, sread)
+	}
+}
 
 // Does not test concurrency, just makes sure the basic read write functionality
 // works
