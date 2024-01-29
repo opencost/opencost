12b268e4240917fd6c8e7804664b09fd373c0263
Artur Khantimirov <yokree@gmail.com>
2024-01-30 09:49:33 +1300

    Stop emitting unused metrics (#2313)
    Stop emitting unused metrics by default.
    Significantly reduce CPU/Memory usage.
    
    ---------
    
    Signed-off-by: r2k1 <yokree@gmail.com>
    Co-authored-by: Ajay Tripathy <ajay@kubecost.com>

--- pkg/costmodel/metrics.go
+++ pkg/costmodel/metrics.go
@@ -345,6 +345,7 @@ func NewCostModelMetricsEmitter(promClient promclient.Client, clusterCache clust
 		EmitPodAnnotations:            env.IsEmitPodAnnotationsMetric(),
 		EmitKubeStateMetrics:          env.IsEmitKsmV1Metrics(),
 		EmitKubeStateMetricsV1Only:    env.IsEmitKsmV1MetricsOnly(),
+		EmitDeprecatedMetrics:         env.IsEmitDeprecatedMetrics(),
 	})
 
 	metrics.InitOpencostTelemetry(metricsConfig)
