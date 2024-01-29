f6bad635633d3b3cf81c8c8986edf89a7033975d
Michael Dresser <michael@kubecost.com>
2024-01-29 15:08:25 -0800

    Make field map access concurrency-safe (#2476)
    * Make field map access concurrency-safe
    
    Signed-off-by: Michael Dresser <michaelmdresser@gmail.com>
    
    * Move fieldMap init to init()
    
    Signed-off-by: Michael Dresser <michaelmdresser@gmail.com>
    
    ---------
    
    Signed-off-by: Michael Dresser <michaelmdresser@gmail.com>

--- core/pkg/filter/cloudcost/parser.go
+++ core/pkg/filter/cloudcost/parser.go
@@ -17,16 +17,16 @@ var cloudCostFilterFields []*ast.Field = []*ast.Field{
 // fieldMap is a lazily loaded mapping from CloudAggregationField to ast.Field
 var fieldMap map[CloudCostField]*ast.Field
 
+func init() {
+	fieldMap = make(map[CloudCostField]*ast.Field, len(cloudCostFilterFields))
+	for _, f := range cloudCostFilterFields {
+		ff := *f
+		fieldMap[CloudCostField(ff.Name)] = &ff
+	}
+}
+
 // DefaultFieldByName returns only default cloud cost filter fields by name.
 func DefaultFieldByName(field CloudCostField) *ast.Field {
-	if fieldMap == nil {
-		fieldMap = make(map[CloudCostField]*ast.Field, len(cloudCostFilterFields))
-		for _, f := range cloudCostFilterFields {
-			ff := *f
-			fieldMap[CloudCostField(ff.Name)] = &ff
-		}
-	}
-
 	if af, ok := fieldMap[field]; ok {
 		afcopy := *af
 		return &afcopy
