74ac1705344af804564d3e5fc0cd8b74c70aaefd
Matt Ray <github@mattray.dev>
2024-02-07 20:47:15 +1100

    Add footer to Cloud Costs
    Signed-off-by: Matt Ray <github@mattray.dev>

--- ui/src/cloudCostReports.js
+++ ui/src/cloudCostReports.js
@@ -1,6 +1,7 @@
 import * as React from "react";
 import Page from "./components/Page";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import IconButton from "@material-ui/core/IconButton";
 import RefreshIcon from "@material-ui/icons/Refresh";
 import { makeStyles } from "@material-ui/styles";
@@ -328,6 +329,7 @@ const CloudCostReports = () => {
           )}
         </Paper>
       )}
+      <Footer/>
     </Page>
   );
 };
