da2eca1a842294eac8f13b90efe46ff388a2d534
thomasvn <thomasnguyen96@gmail.com>
2024-02-13 12:29:21 -0800

    Prevent overwriting of node labels in the map.
    Signed-off-by: thomasvn <thomasnguyen96@gmail.com>

--- pkg/costmodel/cluster_helpers.go
+++ pkg/costmodel/cluster_helpers.go
@@ -640,7 +640,18 @@ func buildLabelsMap(
 		// ingested label data. This removes the label_ prefix that prometheus
 		// adds to emitted labels. It also keeps from ingesting prometheus labels
 		// that aren't a part of the asset.
-		m[key] = result.GetLabels()
+		// m[key] = result.GetLabels()
+
+		if _, ok := m[key]; !ok {
+			m[key] = map[string]string{}
+		}
+
+		for k, l := range result.GetLabels() {
+			m[key][k] = l
+			// TODO: remove log
+			log.Infof("buildLabelsMap: cluster=%s, node=%s, label=%s, value=%s", cluster, node, k, l)
+		}
+
 	}
 	return m
 }
