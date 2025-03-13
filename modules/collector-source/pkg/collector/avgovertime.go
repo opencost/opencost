a8ca24169884876b2ac308896f80c662ea9dae0c
Matt Bolt <mbolt35@gmail.com>
2025-03-13 12:54:11 -0400

    rename transformer to aggregator

--- modules/collector-source/pkg/collector/avgovertime.go
+++ modules/collector-source/pkg/collector/avgovertime.go
@@ -1,6 +1,6 @@
 package collector
 
-type AverageOverTimeTransformer struct {
+type AverageOverTimeAggregator struct {
 	name        string
 	labelValues []string
 	total       float64
@@ -8,25 +8,25 @@ type AverageOverTimeTransformer struct {
 }
 
 func AverageOverTime(name string, labelValues []string) MetricAggregator {
-	return &AverageOverTimeTransformer{
+	return &AverageOverTimeAggregator{
 		name:        name,
 		labelValues: labelValues,
 	}
 }
 
-func (m *AverageOverTimeTransformer) Name() string {
+func (m *AverageOverTimeAggregator) Name() string {
 	return m.name
 }
 
-func (m *AverageOverTimeTransformer) LabelValues() []string {
+func (m *AverageOverTimeAggregator) LabelValues() []string {
 	return m.labelValues
 }
 
-func (m *AverageOverTimeTransformer) Update(value float64) {
+func (m *AverageOverTimeAggregator) Update(value float64) {
 	m.total += value
 	m.count++
 }
 
-func (m *AverageOverTimeTransformer) Value() float64 {
+func (m *AverageOverTimeAggregator) Value() float64 {
 	return m.total / float64(m.count)
 }
