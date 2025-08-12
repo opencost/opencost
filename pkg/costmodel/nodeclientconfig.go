528d3e04174c600f7c69796809b21e376109ce2b
sneax <paladesh600@gmail.com>
2025-08-12 22:51:16 +0530

    Fix: Update TLS MinVersion in nodeclientconfig.go
    Updated TLS configurations in pkg/costmodel/nodeclientconfig.go to enforce a minimum TLS version of 1.2. This addresses security vulnerabilities related to using outdated TLS protocols.

--- pkg/costmodel/nodeclientconfig.go
+++ pkg/costmodel/nodeclientconfig.go
@@ -59,12 +59,14 @@ func NewNodeClientConfigFromEnv() (*nodes.NodeClientConfig, error) {
 			tlsConfig = &tls.Config{
 				Certificates: []tls.Certificate{cert},
 				RootCAs:      caCertPool,
+				MinVersion:   tls.VersionTLS12,
 			}
 
 			transport = &http.Transport{TLSClientConfig: tlsConfig}
 		} else {
 			tlsConfig := &tls.Config{
 				RootCAs: caCertPool,
+				MinVersion:   tls.VersionTLS12,
 			}
 			transport = &http.Transport{TLSClientConfig: tlsConfig}
 		}
