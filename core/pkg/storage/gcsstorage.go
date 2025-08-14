2e4cbc07bf8cc53ba105896df49804dc6f87f78b
Ishaan Mittal <ishaanmittal123@gmail.com>
2025-08-15 02:21:05 +0530

    nit fix Signed-off-by: Ishaan Mittal <ishaanmittal123@gmail.com>

--- core/pkg/storage/gcsstorage.go
+++ core/pkg/storage/gcsstorage.go
@@ -111,7 +111,7 @@ func (gs *GCSStorage) Stat(name string) (*StorageInfo, error) {
 
 // isDoesNotExist returns true if the error matches resource not exists errors.
 func (gs *GCSStorage) isDoesNotExist(err error) bool {
-	return strings.HasPrefix(err.Error(), gcs.ErrObjectNotExist.Error())
+	return errors.Is(err, gcs.ErrObjectNotExist)
 }
 
 // Read uses the relative path of the storage combined with the provided path to
