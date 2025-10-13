ecf734f2d51d5ea53279a91d4ad98bd37899b2b3
Robert Kessler <rob@cake.ai>
2025-10-13 08:20:36 -0500

    Fix providerID set to node name in idle allocations
    Idle allocations were setting providerID to the Kubernetes node name
    instead of the cloud provider instance ID. This prevented reconciliation
    with cloud billing data which uses instance IDs (e.g., AWS EC2 instance
    IDs, GCP instance IDs, Azure VM IDs).
    
    This commit:
    1. Adds ProviderID field to AssetTotals struct
    2. Populates ProviderID when computing asset totals
    3. Uses ProviderID (not Node) when creating idle allocations
    
    This enables proper reconciliation between OpenCost idle costs and
    cloud provider billing data.
    
    Fixes Issue #3401
    
    Signed-off-by: Robert Kessler <rbtkess@users.noreply.github.com>
    Signed-off-by: Robert Kessler <rob@cake.ai>

--- pkg/costmodel/costmodel.go
+++ pkg/costmodel/costmodel.go
@@ -1796,7 +1796,7 @@ func computeIdleAllocations(allocSet *opencost.AllocationSet, assetSet *opencost
 			Properties: &opencost.AllocationProperties{
 				Cluster:    assetTotal.Cluster,
 				Node:       assetTotal.Node,
-				ProviderID: assetTotal.Node,
+				ProviderID: assetTotal.ProviderID,
 			},
 			Start:   assetTotal.Start,
 			End:     assetTotal.End,
