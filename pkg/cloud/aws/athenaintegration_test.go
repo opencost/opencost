848c732aa893b8a1483dcf2329f14b0d3e4e51b7
Ishaan Mittal <ishaanmittal123@gmail.com>
2026-03-13 21:50:39 +0530

    Add tests Signed-off-by: Ishaan Mittal <ishaanmittal123@gmail.com>

--- pkg/cloud/aws/athenaintegration_test.go
+++ pkg/cloud/aws/athenaintegration_test.go
@@ -70,7 +70,7 @@ func TestAthenaIntegration_GetCloudCost(t *testing.T) {
 }
 
 func Test_athenaRowToCloudCost(t *testing.T) {
-	aqi := AthenaQueryIndexes{
+	aqi_cur_10 := AthenaQueryIndexes{
 		ColumnIndexes: map[string]int{
 			"ListCostColumn":              0,
 			"NetCostColumn":               1,
@@ -97,6 +97,32 @@ func Test_athenaRowToCloudCost(t *testing.T) {
 		IsK8sColumn:            "IsK8sColumn",
 	}
 
+	aqi_cur_20 := AthenaQueryIndexes{
+		ColumnIndexes: map[string]int{
+			"ListCostColumn":                               0,
+			"NetCostColumn":                                1,
+			"AmortizedNetCostColumn":                       2,
+			"AmortizedCostColumn":                          3,
+			"IsK8sColumn":                                  4,
+			AthenaDateTruncColumn:                          5,
+			"line_item_resource_id":                        6,
+			"bill_payer_account_id":                        7,
+			"line_item_usage_account_id":                   8,
+			"line_item_product_code":                       9,
+			"line_item_usage_type":                         10,
+			"product_region_code":                          11,
+			"line_item_availability_zone":                  12,
+			"CAST(resource_tags AS JSON) as resource_tags": 13,
+		},
+		TagColumns:             []string{},
+		AWSTagColumns:          []string{},
+		ListCostColumn:         "ListCostColumn",
+		NetCostColumn:          "NetCostColumn",
+		AmortizedNetCostColumn: "AmortizedNetCostColumn",
+		AmortizedCostColumn:    "AmortizedCostColumn",
+		IsK8sColumn:            "IsK8sColumn",
+	}
+
 	tests := []struct {
 		name    string
 		row     []string
@@ -105,51 +131,51 @@ func Test_athenaRowToCloudCost(t *testing.T) {
 		wantErr bool
 	}{
 		{
-			name:    "incorrect row length",
+			name:    "incorrect row length CUR 1.0",
 			row:     []string{"not enough elements"},
-			aqi:     aqi,
+			aqi:     aqi_cur_10,
 			want:    nil,
 			wantErr: true,
 		},
 		{
-			name:    "invalid list cost",
+			name:    "invalid list cost CUR 1.0",
 			row:     []string{"invalid", "2", "3", "4", "true", "2024-09-01 00:00:00.000", "resourceID", "payerAccountID", "usageAccountID", "productCode", "usageType", "regionCode", "availabilityZone", "userTagTestValue", "awsTagTestValue"},
-			aqi:     aqi,
+			aqi:     aqi_cur_10,
 			want:    nil,
 			wantErr: true,
 		},
 		{
-			name:    "invalid net cost",
+			name:    "invalid net cost CUR 1.0",
 			row:     []string{"1", "invalid", "3", "4", "true", "2024-09-01 00:00:00.000", "resourceID", "payerAccountID", "usageAccountID", "productCode", "usageType", "regionCode", "availabilityZone", "userTagTestValue", "awsTagTestValue"},
-			aqi:     aqi,
+			aqi:     aqi_cur_10,
 			want:    nil,
 			wantErr: true,
 		},
 		{
-			name:    "invalid amortized net cost",
+			name:    "invalid amortized net cost CUR 1.0",
 			row:     []string{"1", "2", "invalid", "4", "true", "2024-09-01 00:00:00.000", "resourceID", "payerAccountID", "usageAccountID", "productCode", "usageType", "regionCode", "availabilityZone", "userTagTestValue", "awsTagTestValue"},
-			aqi:     aqi,
+			aqi:     aqi_cur_10,
 			want:    nil,
 			wantErr: true,
 		},
 		{
-			name:    "invalid amortized cost",
+			name:    "invalid amortized cost CUR 1.0",
 			row:     []string{"1", "2", "3", "invalid", "true", "2024-09-01 00:00:00.000", "resourceID", "payerAccountID", "usageAccountID", "productCode", "usageType", "regionCode", "availabilityZone", "userTagTestValue", "awsTagTestValue"},
-			aqi:     aqi,
+			aqi:     aqi_cur_10,
 			want:    nil,
 			wantErr: true,
 		},
 		{
-			name:    "invalid date",
+			name:    "invalid date CUR 1.0",
 			row:     []string{"1", "2", "3", "4", "true", "invalid", "resourceID", "payerAccountID", "usageAccountID", "productCode", "usageType", "regionCode", "availabilityZone", "userTagTestValue", "awsTagTestValue"},
-			aqi:     aqi,
+			aqi:     aqi_cur_10,
 			want:    nil,
 			wantErr: true,
 		},
 		{
-			name: "valid kubernetes with labels",
+			name: "valid kubernetes with labels CUR 1.0",
 			row:  []string{"1", "2", "3", "4", "true", "2024-09-01 00:00:00.000", "resourceID", "payerAccountID", "usageAccountID", "productCode", "usageType", "regionCode", "availabilityZone", "userTagTestValue", "awsTagTestValue"},
-			aqi:  aqi,
+			aqi:  aqi_cur_10,
 			want: &opencost.CloudCost{
 				Properties: &opencost.CloudCostProperties{
 					ProviderID:        "resourceID",
@@ -197,7 +223,7 @@ func Test_athenaRowToCloudCost(t *testing.T) {
 		{
 			name: "valid non-kubernetes, no labels",
 			row:  []string{"1", "2", "3", "4", "false", "2024-09-01 00:00:00.000", "resourceID", "payerAccountID", "usageAccountID", "productCode", "usageType", "regionCode", "availabilityZone", "", ""},
-			aqi:  aqi,
+			aqi:  aqi_cur_10,
 			want: &opencost.CloudCost{
 				Properties: &opencost.CloudCostProperties{
 					ProviderID:        "resourceID",
@@ -240,9 +266,9 @@ func Test_athenaRowToCloudCost(t *testing.T) {
 			wantErr: false,
 		},
 		{
-			name: "valid load balancer product code",
+			name: "valid load balancer product code CUR 1.0",
 			row:  []string{"1", "2", "3", "4", "false", "2024-09-01 00:00:00.000", "resourceID/lbID", "payerAccountID", "usageAccountID", "AWSELB", "usageType", "regionCode", "availabilityZone", "", ""},
-			aqi:  aqi,
+			aqi:  aqi_cur_10,
 			want: &opencost.CloudCost{
 				Properties: &opencost.CloudCostProperties{
 					ProviderID:        "lbID",
@@ -285,9 +311,9 @@ func Test_athenaRowToCloudCost(t *testing.T) {
 			wantErr: false,
 		},
 		{
-			name: "valid non-kubernetes, Fargate CPU",
+			name: "valid non-kubernetes, Fargate CPU CUR 1.0",
 			row:  []string{"1", "2", "3", "4", "false", "2024-09-01 00:00:00.000", "123:pod/resource", "payerAccountID", "usageAccountID", "AmazonEKS", "CPU", "regionCode", "availabilityZone", "", ""},
-			aqi:  aqi,
+			aqi:  aqi_cur_10,
 			want: &opencost.CloudCost{
 				Properties: &opencost.CloudCostProperties{
 					ProviderID:        "123:pod/resource/CPU",
@@ -330,9 +356,9 @@ func Test_athenaRowToCloudCost(t *testing.T) {
 			wantErr: false,
 		},
 		{
-			name: "valid non-kubernetes, Fargate RAM",
+			name: "valid non-kubernetes, Fargate RAM CUR 1.0",
 			row:  []string{"1", "2", "3", "4", "false", "2024-09-01 00:00:00.000", "123:pod/resource", "payerAccountID", "usageAccountID", "AmazonEKS", "GB", "regionCode", "availabilityZone", "", ""},
-			aqi:  aqi,
+			aqi:  aqi_cur_10,
 			want: &opencost.CloudCost{
 				Properties: &opencost.CloudCostProperties{
 					ProviderID:        "123:pod/resource/RAM",
@@ -370,6 +396,54 @@ func Test_athenaRowToCloudCost(t *testing.T) {
 				AmortizedCost: opencost.CostMetric{
 					Cost:              4,
 					KubernetesPercent: 0,
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "valid kubernetes with labels CUR 2.0",
+			row:  []string{"1", "2", "3", "4", "true", "2024-09-01 00:00:00.000", "resourceID", "payerAccountID", "usageAccountID", "productCode", "usageType", "regionCode", "availabilityZone", `{"test": "userTagTestValue", "aws_test": "awsTagTestValue"}`},
+			aqi:  aqi_cur_20,
+			want: &opencost.CloudCost{
+				Properties: &opencost.CloudCostProperties{
+					ProviderID:        "resourceID",
+					Provider:          "AWS",
+					AccountID:         "usageAccountID",
+					AccountName:       "usageAccountID",
+					InvoiceEntityID:   "payerAccountID",
+					InvoiceEntityName: "payerAccountID",
+					RegionID:          "regionCode",
+					AvailabilityZone:  "availabilityZone",
+					Service:           "productCode",
+					Category:          opencost.OtherCategory,
+					Labels: opencost.CloudCostLabels{
+						"test":     "userTagTestValue",
+						"aws_test": "awsTagTestValue",
+					},
+				},
+				Window: opencost.NewClosedWindow(
+					time.Date(2024, 9, 1, 0, 0, 0, 0, time.UTC),
+					time.Date(2024, 9, 2, 0, 0, 0, 0, time.UTC),
+				),
+				ListCost: opencost.CostMetric{
+					Cost:              1,
+					KubernetesPercent: 1,
+				},
+				NetCost: opencost.CostMetric{
+					Cost:              2,
+					KubernetesPercent: 1,
+				},
+				AmortizedNetCost: opencost.CostMetric{
+					Cost:              3,
+					KubernetesPercent: 1,
+				},
+				InvoicedCost: opencost.CostMetric{
+					Cost:              2,
+					KubernetesPercent: 1,
+				},
+				AmortizedCost: opencost.CostMetric{
+					Cost:              4,
+					KubernetesPercent: 1,
 				},
 			},
 			wantErr: false,
