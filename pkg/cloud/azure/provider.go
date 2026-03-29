94b9715060b3eb082a576fff058feba110e847dc
theycallmeaabie <theycallmeaabie@gmail.com>
2026-03-29 09:39:54 +0530

    fix: close response body in Azure getRetailPrice()
    Signed-off-by: theycallmeaabie <theycallmeaabie@gmail.com>

--- pkg/cloud/azure/provider.go
+++ pkg/cloud/azure/provider.go
@@ -306,6 +306,7 @@ func getRetailPrice(region string, skuName string, currencyCode string, spot boo
 	if err != nil {
 		return "", fmt.Errorf("failed to fetch retail price with URL \"%s\": %v", pricingURL, err)
 	}
+	defer resp.Body.Close()
 
 	if resp.StatusCode < 200 || resp.StatusCode > 299 {
 		return "", fmt.Errorf("retail price responded with error status code %d", resp.StatusCode)
