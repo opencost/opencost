bce19ed373396b7387a813906bbcbc7e7ef896f1
Matt Bolt <mbolt35@gmail.com>
2026-07-07 14:32:11 -0400

    Bingen 0.2.0 Update (#3891)
    Signed-off-by: Alan Rodrigues <alanrodrigues@Alans-MacBook-Pro.local>

--- modules/collector-source/pkg/metric/bingen.go
+++ modules/collector-source/pkg/metric/bingen.go
@@ -3,4 +3,4 @@ package metric
 // @bingen:generate[streamable,stringtable]:UpdateSet
 // @bingen:generate:Update
 
-//go:generate bingen -package=metric -version=1 -buffer=github.com/opencost/opencost/core/pkg/util
+//go:generate bingen -package=metric -version=1
